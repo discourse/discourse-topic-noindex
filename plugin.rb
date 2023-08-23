2f5206197d70c03a212a7030748aabb760d702b5
marstall <marstall@gmail.com>
2023-08-23 17:01:34 -0400

    add FPO noindex button to topic admin menu

--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: discourse-topic-noindex
 # about: TODO
 # version: 0.0.1
-# authors: Discourse
+# authors: chrism
 # url: TODO
 # required_version: 2.7.0
 
 enabled_site_setting :plugin_name_enabled
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+  PLUGIN_NAME = "discourse-topic-noindex"
 end
 
 require_relative "lib/my_plugin_module/engine"
