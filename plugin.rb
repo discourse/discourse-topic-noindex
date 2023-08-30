1c2b1dc03b953ba9431da6cb6433d4ee31e1ca7e
marstall <marstall@gmail.com>
2023-08-30 09:50:40 -0400

    Revert "add FPO noindex button to topic admin menu"
    This reverts commit 2f5206197d70c03a212a7030748aabb760d702b5.

--- plugin.rb
+++ plugin.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-# name: discourse-topic-noindex
+# name: discourse-plugin-name
 # about: TODO
 # version: 0.0.1
-# authors: chrism
+# authors: Discourse
 # url: TODO
 # required_version: 2.7.0
 
 enabled_site_setting :plugin_name_enabled
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-topic-noindex"
+  PLUGIN_NAME = "discourse-plugin-name"
 end
 
 require_relative "lib/my_plugin_module/engine"
