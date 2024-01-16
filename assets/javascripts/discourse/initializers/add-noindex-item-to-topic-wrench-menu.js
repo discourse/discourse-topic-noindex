2324dded1c7f62d435ca09f4b04c361d011cc623
Jarek Radosz <jarek@cvx.dev>
2024-01-16 17:42:12 +0100

    DEV: Update linting (#9)

--- assets/javascripts/discourse/initializers/add-noindex-item-to-topic-wrench-menu.js
+++ assets/javascripts/discourse/initializers/add-noindex-item-to-topic-wrench-menu.js
@@ -1,5 +1,5 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
 import { ajax } from "discourse/lib/ajax";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
 // eslint-disable-next-line no-unused-vars
 const PLUGIN_ID = "discourse-topic-noindex";
